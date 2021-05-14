d15597d72c003727b97f30d3628173fa48c06747
Laila Atrmouh <laila.atrmouh@gmail.com>
2021-05-14 21:06:44 +0200

    :hankey: use environment variable

--- api/members_count.ts
+++ api/members_count.ts
@@ -4,7 +4,7 @@ import insert from './db/insert';
 
 const handler: Handler = async (event, context) => {
   let count = 0;
-  axios.defaults.headers.get['csrf-token'] = 'cbeacf40-4612-4a68-a866-c4288b5aa287';
+  axios.defaults.headers.get['csrf-token'] = process.env.MEETUP_TOKEN;
   await axios.get('https://api.meetup.com/Ladies-of-Code-Paris?').then((response) => {
     console.log(response.data.members);
     count = response.data.members;
