3e960df677ce6cf16a93c684812f5ef4e8f6711d
Laila Atrmouh <laila.atrmouh@gmail.com>
2021-05-15 10:58:29 +0200

    debug

--- api/db/insert.js
+++ api/db/insert.js
@@ -8,15 +8,18 @@ let cachedDb = null;
 // A function for connecting to MongoDB,
 // taking a single parameter of the connection string
 async function connectToDatabase(uri) {
+	console.log(uri);
 	// If the database connection is cached,
 	// use it instead of creating a new connection
 	if (cachedDb) {
+		console.log('cached db');
 		return cachedDb;
 	}
 
 	// If no connection is cached, create a new one
 	const client = await MongoClient.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true});
 
+	console.log(client);
 	// Select the database through the connection,
 	// using the database path of the connection string
 	const db = await client.db(url.parse(uri).pathname.substr(1));
